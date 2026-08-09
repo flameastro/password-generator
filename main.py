6530af7c51e12d1258956be9702e30f9de88ff3a
flameastro <gabrielgoulartbnu@gmail.com>
2026-08-09 15:58:19 -0300

    refactor: add docstrings, is_valid and save_passwords functions

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 import secrets
 
 
-# Todo: add just one function called is_valid_number, that verify if the input number is correct
+def is_valid(number: int, minimum: int, maximum: int):
+    """Returns True if the number is in the correct range between minimum and maximum and False otherwise"""
 
-def is_valid_lenght(length: int):
-    return length >= 6 and length <= 256
-
-
-def is_valid_quantity(quantity: int):
-    return quantity >= 1 and quantity <= 100
+    return number >= minimum and number <= maximum
 
 
-def save_passwords(password):
-    FILE = "passwords.txt"
+def save_passwords(save: bool, password: str):
+    """Allow the program to save the generated password if save is True"""
 
-    with open(FILE, "a+", encoding="utf-8") as f:
-        f.write(f"{password}\n\n")
+    if save:
+        FILE = "passwords.txt"
+
+        with open(FILE, "a+", encoding="utf-8") as f:
+            f.write(f"{password}\n\n")
 
 
 def allowed_characters(numbers: bool = True, special: bool = True):
+    """Returns the allowed characters to generate the final password"""
+
     UPPERCASE = "abcdefghijklmnopqrstuvwxyz"
     LOWERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     NUMBERS = "0123456789"
@@ -34,6 +35,8 @@ def allowed_characters(numbers: bool = True, special: bool = True):
 
 
 def generate_encrypt_password(length: int, characters: str):
+    """Generate the password with secrets library"""
+
     password = ""
     for _ in range(length):
         password += secrets.choice(characters)
@@ -41,28 +44,27 @@ def generate_encrypt_password(length: int, characters: str):
     return password
 
 
-
 def main():
+    """Execute the all the functions and make it interactive to the user :)"""
+
     length = int(input("Enter the password length (between 1 and 256): "))
 
-    if is_valid_lenght(length):
+    if is_valid(length, 6, 256):
         numbers = bool(input("Allow numbers? (Type any key and press ENTER for yes, or leave blank): "))
         special = bool(input("Allow special characters? Example: !@#$%^&*()-_=+? (Type any key and press ENTER if yes, or leave blank if no): "))
 
         characters = allowed_characters(numbers, special)
 
         quantity = int(input(f"How many passwords do you want to generate with a length of {length} characters? (Between 1 and 100): "))
-        if is_valid_quantity(quantity):
+        if is_valid(quantity, 1, 100):
             save = bool(f"Do you want to save the {quantity} generated passwords to a file named senhas.txt? (Type any key and press ENTER to save, or leave blank if not): ")
 
             for _ in range(quantity):
                 password = generate_encrypt_password(length, characters)
                 print(password)
 
-                if save:
-                    save_passwords(password)
+                save_passwords(save, password)
     else:
         print("The password length must be between 6 and 256 characters. Please try again.")
-
 
 main()
